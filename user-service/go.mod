4e36e44317ac7c426c5bc934f51bdadf8eb3dc9c
marsli9945 <marsli9945@gmail.com>
2020-03-10 19:29:47 +0800

    依赖修改

--- user-service/go.mod
+++ user-service/go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.4
-	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/jinzhu/gorm v1.9.12
 	github.com/micro/go-micro v1.18.0
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
+	golang.org/x/net v0.0.0-20191109021931-daa7c04131f5
 )
--- user-service/handler/handler.go
+++ user-service/handler/handler.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
-	pb "github.com/nonfu/laracom/user-service/proto/user"
-	"github.com/nonfu/laracom/user-service/repo"
+	pb "github.com/marsli9945/laracom/user-service/proto/user"
+	"github.com/marsli9945/laracom/user-service/repo"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/context"
 )
