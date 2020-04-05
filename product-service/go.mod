a2ecb97caae448bf0a334cbb4149e2be28041d1f
marsli9945 <marsli9945@gmail.com>
2020-04-05 17:25:05 +0800

    商品服务新增相关表和接口

--- product-service/go.mod
+++ product-service/go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/golang/protobuf v1.3.5
 	github.com/jinzhu/gorm v1.9.12
 	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-plugins/wrapper/monitoring/prometheus v0.0.0-20200119172437-4fe21aa238fd
+	github.com/prometheus/client_golang v1.3.0
 )
--- product-service/main.go
+++ product-service/main.go
@@ -8,8 +8,22 @@ import (
 	pb "github.com/marsli9945/laracom/product-service/proto/product"
 	"github.com/marsli9945/laracom/product-service/repo"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-plugins/wrapper/monitoring/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"net/http"
 )
+
+// 启动 HTTP 服务监听客户端数据采集
+func prometheusBoot() {
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(":9092", nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	}()
+}
 
 func main() {
 	// 创建数据库连接，程序退出时断开连接
@@ -21,6 +35,7 @@ func main() {
 	}
 
 	// 数据库迁移
+	database.Set("gorm:table_options", "charset=utf8")
 	database.AutoMigrate(&model.Product{})
 	database.AutoMigrate(&model.ProductImage{})
 	database.AutoMigrate(&model.Brand{})
@@ -30,25 +45,29 @@ func main() {
 	database.AutoMigrate(&model.ProductAttribute{})
 
 	// 初始化 Repo 实例用于后续数据库操作
-	productRepo := &repo.ProductRepository{database}
-	imageRepo := &repo.ImageRepository{database}
-	brandRepo := &repo.BrandRepository{database}
-	categoryRepo := &repo.CategoryRepository{database}
-	attributeRepo := &repo.AttributeRepository{database}
+	productRepo := &repo.ProductRepository{Db: database}
+	imageRepo := &repo.ImageRepository{Db: database}
+	brandRepo := &repo.BrandRepository{Db: database}
+	categoryRepo := &repo.CategoryRepository{Db: database}
+	attributeRepo := &repo.AttributeRepository{Db: database}
 
 	// 以下是 Micro 创建微服务流程
 	srv := micro.NewService(
 		micro.Name("laracom.service.product"),
 		micro.Version("latest"), // 新增接口版本参数
+		micro.WrapHandler(prometheus.NewHandlerWrapper()),
 	)
 	srv.Init()
 
 	// 注册处理器
-	pb.RegisterProductServiceHandler(srv.Server(), &handler.ProductService{productRepo})
-	pb.RegisterImageServiceHandler(srv.Server(), &handler.ImageService{imageRepo})
-	pb.RegisterBrandServiceHandler(srv.Server(), &handler.BrandService{brandRepo})
-	pb.RegisterCategoryServiceHandler(srv.Server(), &handler.CategoryService{categoryRepo})
-	pb.RegisterAttributeServiceHandler(srv.Server(), &handler.AttributeService{attributeRepo})
+	_ = pb.RegisterProductServiceHandler(srv.Server(), &handler.ProductService{ProductRepo: productRepo})
+	_ = pb.RegisterImageServiceHandler(srv.Server(), &handler.ImageService{ImageRepo: imageRepo})
+	_ = pb.RegisterBrandServiceHandler(srv.Server(), &handler.BrandService{BrandRepo: brandRepo})
+	_ = pb.RegisterCategoryServiceHandler(srv.Server(), &handler.CategoryService{CategoryRepo: categoryRepo})
+	_ = pb.RegisterAttributeServiceHandler(srv.Server(), &handler.AttributeService{AttributeRepo: attributeRepo})
+
+	// 采集监控数据
+	prometheusBoot()
 
 	// 启动商品服务
 	if err := srv.Run(); err != nil {
